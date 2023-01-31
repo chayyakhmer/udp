9df4deba580cf85300919060cb24e3da416d1915
chayyakhmer <109713546+chayyakhmer@users.noreply.github.com>
2023-01-31 17:37:39 +0700

    Update runudp.sh

--- runudp.sh
+++ runudp.sh
@@ -1,1 +1,1 @@
-/data/data/com.termux/files/home/udp/udpopenvpn -u -s 188.166.233.86 -c openvpnudp.json
+/data/data/com.termux/files/home/udp/udpopenvpn -u -s 188.166.233.86 -c /data/data/com.termux/files/home/udp/openvpnudp.json
